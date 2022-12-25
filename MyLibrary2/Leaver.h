091206dbd02ccc1591b569097e750346578368be
Даниил <derendervad@gmail.com>
2022-12-25 13:08:46 +0300

    change

--- MyLibrary2/Leaver.h
+++ MyLibrary2/Leaver.h
@@ -1,14 +1,13 @@
 #pragma once
 #include <iostream>
-#ifdef MyLibrary_API_EXPORTS
-#define MyLibrary_API __declspec(dllexport)
+#ifdef MYLIBRARY2_EXPORTS
+#define MYLIBRARY2 __declspec(dllexport)
 #else
-#define MyLibrary_API __declspec(dllimport)
+#define MYLIBRARY2 __declspec(dllimport)
 #endif
 class Leaver
 {
 public:
-	MyLibrary_API std::string leave(std::string str);
-	//MyLinrary2_API std::string leave(std::string str); Выдает ошибку warning C4273: Leaver::leave: несовместимое связывание dll
+	MYLIBRARY2 std::string leave(std::string str);
 };
 
